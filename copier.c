1a9347ceabd72df48db65d9e483d530722049e22
Axolot <147263782+Axolot1337@users.noreply.github.com>
2026-05-30 01:40:48 +0300

    Add copier.c - main C source file

---
+++ copier.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+
+int main(void)
+{
+    char memory[4096];
+    char source[4096];
+    char destination[4096];
+    FILE *source_ptr;
+    FILE *destination_ptr;
+
+    printf("Enter the directory where the file will be copied: ");
+    fgets(source, sizeof(source), stdin);
+    source[strcspn(source, "\n")] = 0;
+
+    source_ptr = fopen(source, "r");
+    if (source_ptr == NULL) {
+        printf("Error: %s\n", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Enter the directory where the file will be pasted: ");
+    fgets(destination, sizeof(destination), stdin);
+    destination[strcspn(destination, "\n")] = 0;
+
+    destination_ptr = fopen(destination, "w");
+    if (destination_ptr == NULL) {
+        printf("Error: %s\n", strerror(errno));
+        fclose(source_ptr);
+        source_ptr = NULL;
+        exit(EXIT_FAILURE);
+    }
+
+    while(fgets(memory, sizeof(memory), source_ptr)) {
+        fputs(memory, destination_ptr);
+    }
+
+    fclose(source_ptr);
+    source_ptr = NULL;
+    fclose(destination_ptr);
+    destination_ptr = NULL;
+
+    printf("File successfully copied from '%s' to '%s'\n", source, destination);
+    return 0;
+}
